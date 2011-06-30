bfe5ad5e6bf50c6df274b46dbd767cfc1d3d53f6
Stephen <stephennancekivell@gmail.com>
2011-07-01 00:57:18 +1200

    initish

---
+++ GmailSend.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import smtplib,sys
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import argparse
+
+def send(user,pwd,to, subject, text):
+   msg = MIMEMultipart()
+
+   msg['From'] = user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(user, pwd)
+   mailServer.sendmail(user, to, msg.as_string())
+   mailServer.close()
+
+def main():
+	desc = "To send emails from a gmail account."
+	parser = argparse.ArgumentParser(description = desc)
+
+	parser.add_argument("-u", "--user", dest='user', required=True, help="The gmail account to send from")
+	parser.add_argument("-p", "--password", dest='password', required=True)
+	parser.add_argument("-t", "--to", dest="to", required=True)
+	parser.add_argument("-s", "--subject", dest="subject", default="")
+	parser.add_argument("-b", "--body", dest="body", default="")
+
+	a = parser.parse_args()
+	send(a.user, a.password, a.to, a.subject, a.body)
+
+if __name__ == "__main__":
+	main()
---
+++ readme.md
@@ -0,0 +1,17 @@
+Send email messages from the command line using a gmail account. Intended for automated notification from scripts.
+
+# Usage
+usage: GmailSend.py [-h] -u USER -p PASSWORD -t TO [-s SUBJECT] [-b BODY]
+
+To send emails from a gmail account.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u USER, --user USER  The gmail account to send from
+  -p PASSWORD, --password PASSWORD
+  -t TO, --to TO
+  -s SUBJECT, --subject SUBJECT
+  -b BODY, --body BODY
+
+# Security note
+Passwords in scripts are stored in plan text, dont use a email account that you dont want compromised.
